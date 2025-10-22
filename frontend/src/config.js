aea7cf026ad80a91af31ee645122a6bb7ec2257f
Nextefer <aimars123@outlook.com>
2025-10-22 17:26:57 -0600

    first commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // Define la URL base de tu API.
 // Cambia esta variable si la IP o el puerto de tu backend cambian.
-const API_BASE_URL = 'http://192.168.0.73:5000';
+const API_BASE_URL = 'http://192.168.0.107:5000';
 
 export default API_BASE_URL;
